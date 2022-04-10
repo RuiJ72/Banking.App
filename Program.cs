f4716abbb5361552f22aa1d9dd8b8f7b8229e282
RuiJ72 <rlagos900@gmail.com>
2022-04-10 14:00:43 -0300

    Deleção de comentário em função da alteração do código

--- Program.cs
+++ Program.cs
@@ -6,15 +6,9 @@ namespace Banking.App.Conta
     {
         static void Main(string[] args) 
         {
-           Conta minhaConta = new Conta();
-           minhaConta.Nome = "João";
-
-           
-
-           System.Console.WriteLine("");
+            System.Console.WriteLine("");
         }
     }
 }
 
 // Na linha 9 instanciamos o nosso objeto
-// Na linha 10 atribuição do nome à conta

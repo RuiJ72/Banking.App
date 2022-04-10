716a7908b4b3da5c27a6d4d79682656886d90ad8
RuiJ72 <rlagos900@gmail.com>
2022-04-10 18:44:38 -0300

    Implementações e correção de alguns bugs

--- Conta/Conta.cs
+++ Conta/Conta.cs
@@ -1,27 +1,68 @@
+using System;
+
 namespace Banking.App.Conta
 {
-    public class Conta
-    {
-       private TipoConta TipoConta { get; set; }
+	public class Conta
+	{
+		// Atributos
+		private TipoConta TipoConta { get; set; }
+		private double Saldo { get; set; }
+		private double Credito { get; set; }
+		private string Nome { get; set; }
 
-       private double Saldo { get; set; }
+		// Métodos
+		public Conta(TipoConta tipoConta, double saldo, double credito, string nome)
+		{
+			this.TipoConta = tipoConta;
+			this.Saldo = saldo;
+			this.Credito = credito;
+			this.Nome = nome;
+		}
 
-       private double Credito { get; set; }
+		public bool Sacar(double valorSaque)
+		{
+            // Validação de saldo suficiente
+            if (this.Saldo - valorSaque < (this.Credito *-1)){
+                Console.WriteLine("Saldo insuficiente!");
+                return false;
+            }
+            this.Saldo -= valorSaque;
 
-       private string Nome  { get; set; }
+            Console.WriteLine("Saldo atual da conta de {0} é {1}", this.Nome, this.Saldo);
+            
 
+            return true;
+		}
 
-        // Método construtor
-       public Conta(TipoConta tipoConta, double saldo, double credito, string nome)
-       {
-           this.TipoConta = tipoConta;
-           this.Saldo = saldo;
-           this.Credito = Credito;
-           this.Nome = nome;
-       }
-    }
+		public void Depositar(double valorDeposito)
+		{
+			this.Saldo += valorDeposito;
+
+            Console.WriteLine("Saldo atual da conta de {0} é {1}", this.Nome, this.Saldo);
+		}
+
+		public void Transferir(double valorTransferencia, Conta contaDestino)
+		{
+			if (this.Sacar(valorTransferencia)){
+                contaDestino.Depositar(valorTransferencia);
+            }
+		}
+
+        public override string ToString()
+		{
+            string retorno = "";
+            retorno += "TipoConta " + this.TipoConta + " | ";
+            retorno += "Nome " + this.Nome + " | ";
+            retorno += "Saldo " + this.Saldo + " | ";
+            retorno += "Crédito " + this.Credito;
+			return retorno;
+		}
+	}
 }
+
 
 // Classe conta com os seus atributos
 /* Como não é desejável nenhum usuário alterar esses dados, os atributos
 são privados, o chamado Encapsulamento */
+
+// linha 34 o {0} e {1} correspondem a um vetor de dados. {0} ehis.Nome e {1} this.Saldo

8aac1da0e03980f66f67c28e2c33cc68df455015
RuiJ72 <rlagos900@gmail.com>
2022-04-10 19:01:16 -0300

    Ajustes Finais

--- Enum/TipoConta.cs
+++ Enum/TipoConta.cs
@@ -1,14 +1,11 @@
 namespace Banking.App
 {
-    
+
+
     public enum TipoConta
     {
-
-    PessoaFisica = 1,
-
-    PessoaJuridica = 2
-
+        PessoaFisica = 1,
+        PessoaJuridica = 2
     }
-
 }
 //Com um enum, temos maior controle e o usuário pode ter mais itens
